8636d524b726559c56517933ceaf383fcc239f75
Willie M. Bonavente <76456752+williebonavente@users.noreply.github.com>
2024-09-03 23:05:50 +0800

    Update the portfolio

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert("Welcome to my Portfolio");
+function copyPaste() {
+    var clipboard = new ClipboardJS('#copyemail');
+    clipboard.on('success', function (_e) {
+        // Change button text to "Copied"
+        $('#copyemail').text('Email Copied');
+
+        // Reset button text to "Copy" after 2 seconds
+        setTimeout(function () {
+            $('#copyemail').text('williembonavente@iskolarngbayan.pup.edu.ph');
+        }, 2000);
+    });
+};
+
+copyPaste();
